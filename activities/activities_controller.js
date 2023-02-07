7b87fef1aba9239b7d048b01b484f9b9118622a3
jguacase <jguacase@emeal.nttdata.com>
2023-02-07 17:06:14 -0500

    Subida evaluacion

--- activities/activities_controller.js
+++ activities/activities_controller.js
@@ -1,7 +1,7 @@
 const Activities = require('./activities_dao');
 /** @function createActivity */
 // Create the specific elements for activity in mongo. 
-exports.createActivity = (req, res, next) => {
+exports.createActivity = (req,res,next)=>{
     const newActivities = {
         id_actividad: req.body.id_actividad,
         cont: req.body.cont,
@@ -46,99 +46,82 @@ exports.createActivity = (req, res, next) => {
         CA3: req.body.CA3,
         evaluacion: req.body.evaluacion,
         descripcion_evaluacion: req.body.descripcion_evaluacion,
-        EQ1: req.body.EQ1,
-        EA11: req.body.EA11,
-        EA12: req.body.EA12,
-        EA13: req.body.EA13,
-        EA14: req.body.EA14,
-        ECA1: req.body.ECA1,
-        EQ2: req.body.EQ2,
-        EA21: req.body.EA21,
-        EA22: req.body.EA22,
-        EA23: req.body.EA23,
-        EA24: req.body.EA24,
-        ECA2: req.body.ECA2,
-        EQ3: req.body.EQ3,
-        EA31: req.body.EA31,
-        EA32: req.body.EA32,
-        EA33: req.body.EA33,
-        EA34: req.body.EA34,
-        ECA3: req.body.ECA3,
+        questions: req.body.preguntas,
         autor: req.body.autor,
         id_autor: req.body.id_autor
     }
     //console.log(newActivities);
-    Activities.create(newActivities, (err, activity) => {
-        if (err) return res.json({ Estado: "Error Crear Actividad" });
-        res.send({ activity });
+    Activities.create(newActivities,(err,activity)=>{
+        if (err) return res.json({Estado: "Error Crear Actividad"});
+        res.send({activity});
     })
 }
 /** @function loadActivity */
 // Load the specific elements for activity in mongo. 
 
-exports.loadActivity = (req, res, next) => {
-    const activityData = {
+exports.loadActivity = (req, res, next)=>{
+    const activityData={
         id_actividad: req.body.id_actividad,
     }
     //console.log(activityData);
-    Activities.findOne({ id_actividad: activityData.id_actividad }, (err, activity) => {
-        if (err) return res.status(500).send('Server Error');
-        if (!activity) {
-            res.status(409).send({ message: 'Something Error' });
-        } else {
-            res.send({ activity });
+    Activities.findOne({id_actividad: activityData.id_actividad}, (err, activity)=>{
+        if(err) return res.status(500).send('Server Error');
+        if(!activity){
+            res.status(409).send({message:'Something Error'});
+        } else{
+            res.send({activity});
         }
     })
 }
 /** @function allActivities */
 // Load all activities of mongo. 
 
-exports.allActivities = (req, res, next) => {
-    Activities.find(function (err, activities) {
-        if (err) return res.status(500).send('Server Error');
-        if (!activities) {
-            res.status(409).send({ message: 'Something Error' });
-        } else {
+exports.allActivities = (req,res,next)=>{
+    Activities.find(function(err, activities){
+        if(err) return res.status(500).send('Server Error');
+        if(!activities){
+            res.status(409).send({message:'Something Error'});
+        } else{
             res.send(activities);
         }
     })
 }
 /** @function allActivitiesMovil */
 // Load all activities of mongo for devices mobile. 
-exports.allActivitiesMovil = async (req, res, next) => {
+exports.allActivitiesMovil = async(req,res,next)=>{
     const studentData = {
         id_grado: req.body.id_grado,
         id_colegio: req.body.id_colegio
     }
-    const data = await Activities.find(function (err, activities) {
-        if (err) return res.status(500).send('Server Error');
-        if (!activities) {
-            res.status(409).send({ message: 'Something Error' });
-        } else {
+    const data = await Activities.find(function(err, activities){
+        if(err) return res.status(500).send('Server Error');
+        if(!activities){
+            res.status(409).send({message:'Something Error'});
+        } else{
             //res.send(activities);
         }
     })
     console.log(data.length);
-    arrayColegio = [];
-    arrayFilterFinal = [];
-    arrayFilterMateria = [];
-    for (var i = 0; i < data.length; i++) {
-        if (data[i].id_colegio == studentData.id_colegio) {
-            arrayColegio.push(data[i]);
+        arrayColegio = [];
+        arrayFilterFinal= [];
+        arrayFilterMateria=[];
+        for (var i=0; i< data.length; i++) {
+            if (data[i].id_colegio == studentData.id_colegio){
+                arrayColegio.push(data[i]);
+            }
         }
-    }
-    console.log(arrayColegio[0].id_grado);
-    for (var j = 0; j < arrayColegio.length; j++) {
-        if (arrayColegio[j].id_grado == studentData.id_grado) {
-            arrayFilterFinal.push(arrayColegio[j]);
+        console.log(arrayColegio[0].id_grado);
+        for (var j=0; j<arrayColegio.length;j++){
+            if (arrayColegio[j].id_grado == studentData.id_grado){
+                arrayFilterFinal.push(arrayColegio[j]);
+            }
         }
-    }
-    res.send(arrayFilterFinal);
+        res.send(arrayFilterFinal);
 }
 /** @function uploadActivity */
 // Update activity of mongo for app web. 
 exports.uploadActivity = async (req, res) => {
-    const activityData = {
+    const activityData={
         id_actividad: req.body.id_actividad,
     }
     const activityNewData = {
@@ -202,22 +185,22 @@ exports.uploadActivity = async (req, res) => {
         EA34: req.body.EA34,
         ECA3: req.body.ECA3
     }
-    await Activities.updateOne({ id_actividad: activityData.id_actividad }, { $set: activityNewData }, { new: true }, (err => {
-        return res.json({ status: 'Actividad Actualizada' });
+    await Activities.updateOne({id_actividad: activityData.id_actividad}, {$set: activityNewData}, {new: true}, (err =>{
+        return res.json({status: 'Actividad Actualizada'});
     }));
 }
 /** @function updloadSectionsActivity */
 // Update activity of mongo for app web. 
 exports.uploadSectionsActivity = async (req, res) => {
-    const activityData = {
+    const activityData={
         id_actividad: req.body.id_actividad,
     }
     const activityNewData = {
         taller: req.body.taller,
         evaluacion: req.body.evaluacion
     }
-    await Activities.updateOne({ id_actividad: activityData.id_actividad }, { $set: activityNewData }, { new: true }, (err => {
-        return res.json({ status: 'Seccion de la Actividad Actualizada' });
+    await Activities.updateOne({id_actividad: activityData.id_actividad}, {$set: activityNewData}, {new: true}, (err =>{
+        return res.json({status: 'Seccion de la Actividad Actualizada'});
     }));
 }
 /** @function deleteActivity */
@@ -227,11 +210,11 @@ exports.deleteActivity = async (req, res) => {
     const activityData = {
         id_actividad: req.body.id_actividad
     }
-    await Activities.deleteOne({ id_actividad: activityData.id_actividad }, (err => {
-        return res.json({ Estado: 'Actividad Eliminada' });
+    await Activities.deleteOne({id_actividad: activityData.id_actividad}, (err =>{
+        return res.json({Estado: 'Actividad Eliminada' });
     }));
 }
 
 
-//id_actividad	id_colegio	id_docente	id_materia	id_competencia	titulo_actividad
+//id_actividad	id_colegio	id_docente	id_materia	id_competencia	titulo_actividad	
 //descripcion_materia	video	urlvideo	lectura	urllectura	test	html	urlhtml	grado
