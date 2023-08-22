f82de56590aaa7973328912a0532c64371cca0bc
Fabinton Sotelo Gomez <49871491+Fabinton@users.noreply.github.com>
2023-08-22 17:14:28 -0500

    Delete comentarios directory

--- comentarios/comentario_routes.js
+++
@@ -1,7 +0,0 @@
-const Comentario = require('./comentario_controller');
-/** @function RoutesComentario */
-// Routes for Countent
-module.exports=(router)=> {
-    router.post('/createComentario', Comentario.createComentario);
-    router.get('/loadComentarios/:id_CREA', Comentario.loadComentarios);
-}
